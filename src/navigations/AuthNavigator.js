061cf2e13d6ff7b5a4e6c2005494f5650da9ae7d
Chris Ardon <rockrisbl@gmail.com>
2021-10-19 00:09:04 -0600

    SignUp to Register

--- src/navigations/AuthNavigator.js
+++ src/navigations/AuthNavigator.js
@@ -3,14 +3,14 @@ import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
 import { LOGIN, REGISTER } from "../constants/routeNames";
 import Login from "../screens/Login/index";
-import SignUp from "../screens/Register/index";
+import Register from "../screens/Register/index";
 
 const AuthNavigator = () => {
     const AuthStack = createStackNavigator();
     return(
         <AuthStack.Navigator>
             <AuthStack.Screen name={LOGIN} component={Login}></AuthStack.Screen>
-            <AuthStack.Screen name={REGISTER} component={SignUp}></AuthStack.Screen>
+            <AuthStack.Screen name={REGISTER} component={Register}></AuthStack.Screen>
         </AuthStack.Navigator>
     );
 }
